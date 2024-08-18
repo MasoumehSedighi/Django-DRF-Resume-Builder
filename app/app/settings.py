2f2f18262ad8d78ecc5a7f00e553a7b2383779ba
MasoumehSedighi <mas_cutt4@yahoo.com>
2024-08-18 21:51:35 +1000

    Add test user model

--- app/app/settings.py
+++ app/app/settings.py
@@ -78,9 +78,9 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_NAME'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD':'postgres',
         'HOST': 'db',
         'PORT': 5432,
     }
